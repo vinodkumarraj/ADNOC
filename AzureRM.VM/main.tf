dddd8c0f37dd991fdfaa455ce7ba818d1dfc7e78
deddivinod <85387939+deddivinod@users.noreply.github.com>
2021-07-12 09:21:50 +0530

    Update main.tf

--- AzureRM.VM/main.tf
+++ AzureRM.VM/main.tf
@@ -21,19 +21,6 @@ resource "azurerm_windows_virtual_machine" "VM" {
   network_interface_ids = [
     azurerm_network_interface.nic.id
   ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-
   tags = {
 		Project = var.project_prefix
 		Environment = var.env_prefix
